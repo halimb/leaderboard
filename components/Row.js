9f83e06228d7e38edcec8226bfed2f5d1ff48018
Halim <halim.bouguedra@gmail.com>
2017-07-26 18:35:55 +0100

    update href urls

--- components/Row.js
+++ components/Row.js
@@ -4,11 +4,11 @@ import { User } from "./User.js";
 export class Row extends React.Component {
 	render() {
 		var user = this.props.data;
-		var url = "https://wwww.freecodecamp.com/" + user.username;
+		var url = "https://www.freecodecamp.com/" + user.username;
 		return (
 			<div className="row">
 				<div className="field rank">{this.props.rank}</div>
-				<a className="field user" href="https://www.google.com" target="_blank" >
+				<a className="field user" href={url} target="_blank" >
 					<User name={user.username} src={user.img}/>
 				</a>
 				<div className="field">{user.recent}</div>
